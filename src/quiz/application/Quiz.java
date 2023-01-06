27ef048c3620ad85ed7eff8fb49e60a141ebdef9
shalexandeer <shalexandeer@student.telkomuniversity.ac.id>
2023-01-06 20:48:25 +0700

    coomit

--- src/quiz/application/Quiz.java
+++ src/quiz/application/Quiz.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Quiz extends JFrame implements ActionListener {
-    
+
     String questions[][] = new String[10][5];
     String answers[][] = new String[10][2];
     String useranswers[][] = new String[10][1];
@@ -13,136 +13,136 @@ public class Quiz extends JFrame implements ActionListener {
     JRadioButton opt1, opt2, opt3, opt4;
     ButtonGroup groupoptions;
     JButton next, submit, lifeline;
-    
-    public static int timer = 15;
+
+    public static int timer = 10;
     public static int ans_given = 0;
     public static int count = 0;
     public static int score = 0;
-    
+
     String name;
-    
+
     Quiz(String name) {
         this.name = name;
         setBounds(50, 0, 1440, 850);
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
-        
+
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/quiz.jpg"));
         JLabel image = new JLabel(i1);
         image.setBounds(0, 0, 1440, 392);
         add(image);
-        
+
         qno = new JLabel();
         qno.setBounds(100, 450, 50, 30);
         qno.setFont(new Font("Tahoma", Font.PLAIN, 24));
         add(qno);
-        
+
         question = new JLabel();
         question.setBounds(150, 450, 900, 30);
         question.setFont(new Font("Tahoma", Font.PLAIN, 24));
         add(question);
-        
-        questions[0][0] = "Which is used to find and fix bugs in the Java programs.?";
-        questions[0][1] = "JVM";
-        questions[0][2] = "JDB";
-        questions[0][3] = "JDK";
-        questions[0][4] = "JRE";
 
-        questions[1][0] = "What is the return type of the hashCode() method in the Object class?";
-        questions[1][1] = "int";
-        questions[1][2] = "Object";
-        questions[1][3] = "long";
-        questions[1][4] = "void";
+        questions[0][0] = "Lapisan kulit bumi yang tersusun atas logam silisium dan aluminium disebut lapisan....";
+        questions[0][1] = "a. sima";
+        questions[0][2] = "b. sial";
+        questions[0][3] = "c. beku";
+        questions[0][4] = "d. karst";
 
-        questions[2][0] = "Which package contains the Random class?";
-        questions[2][1] = "java.util package";
-        questions[2][2] = "java.lang package";
-        questions[2][3] = "java.awt package";
-        questions[2][4] = "java.io package";
+        questions[1][0] = "Batuan yang menglami perubahan yang dahsyat dinamakan....";
+        questions[1][1] = "a. metamorf";
+        questions[1][2] = "b. moraine";
+        questions[1][3] = "c. andesit";
+        questions[1][4] = "d. sedimen";
 
-        questions[3][0] = "An interface with no fields or methods is known as?";
-        questions[3][1] = "Runnable Interface";
-        questions[3][2] = "Abstract Interface";
-        questions[3][3] = "Marker Interface";
-        questions[3][4] = "CharSequence Interface";
+        questions[2][0] = "Tenaga yang banyak merusak bentuk-bentuk permukaan bumi adalah tenaga....";
+        questions[2][1] = "a. alam";
+        questions[2][2] = "b. bumi";
+        questions[2][3] = "c. matahari";
+        questions[2][4] = "d. eksogen";
 
-        questions[4][0] = "In which memory a String is stored, when we create a string using new operator?";
-        questions[4][1] = "Stack";
-        questions[4][2] = "String memory";
-        questions[4][3] = "Random storage space";
-        questions[4][4] = "Heap memory";
+        questions[3][0] = "Peristiwa yang berhubungan dengan naiknya magma dari dalam perut bumi disebut....";
+        questions[3][1] = "a. antikinal";
+        questions[3][2] = "b. tektogenesa";
+        questions[3][3] = "c. vulkanisme";
+        questions[3][4] = "d. diatropisme";
 
-        questions[5][0] = "Which of the following is a marker interface?";
-        questions[5][1] = "Runnable interface";
-        questions[5][2] = "Remote interface";
-        questions[5][3] = "Readable interface";
-        questions[5][4] = "Result interface";
+        questions[4][0] = "Memiliki kemiringan 3%-15% merupakan lahan potensial di daerah....";
+        questions[4][1] = "a. pantai";
+        questions[4][2] = "b. gunung";
+        questions[4][3] = "c. dataran rendah";
+        questions[4][4] = "d. dataran tinggi";
 
-        questions[6][0] = "Which keyword is used for accessing the features of a package?";
-        questions[6][1] = "import";
-        questions[6][2] = "package";
-        questions[6][3] = "extends";
-        questions[6][4] = "export";
+        questions[5][0] = "Suatu tempat yang dapat mengeluarkan CO2 pada gunung api disebut...";
+        questions[5][1] = "a. solfatar";
+        questions[5][2] = "b. mofet";
+        questions[5][3] = "c. fumarol";
+        questions[5][4] = "d. uap air";
 
-        questions[7][0] = "In java, jar stands for?";
-        questions[7][1] = "Java Archive Runner";
-        questions[7][2] = "Java Archive";
-        questions[7][3] = "Java Application Resource";
-        questions[7][4] = "Java Application Runner";
+        questions[6][0] = "Tipe erupsi yang  bekerja dengan irama tiap lima menit mengeluarkan lava dan bom adalah...";
+        questions[6][1] = "a. st. Vincent";
+        questions[6][2] = "b. hawaii";
+        questions[6][3] = "c. stromboli";
+        questions[6][4] = "d. perret";
 
-        questions[8][0] = "Which of the following is a mutable class in java?";
-        questions[8][1] = "java.lang.StringBuilder";
-        questions[8][2] = "java.lang.Short";
-        questions[8][3] = "java.lang.Byte";
-        questions[8][4] = "java.lang.String";
+        questions[7][0] = "Mata air mineral yang biasanya panas adalah..";
+        questions[7][1] = "a. solfatar";
+        questions[7][2] = "b. mofet";
+        questions[7][3] = "c. makdani";
+        questions[7][4] = "d. uap air";
 
-        questions[9][0] = "Which of the following option leads to the portability and security of Java?";
-        questions[9][1] = "Bytecode is executed by JVM";
-        questions[9][2] = "The applet makes the Java code secure and portable";
-        questions[9][3] = "Use of exception handling";
-        questions[9][4] = "Dynamic binding between objects";
-        
-        answers[0][1] = "JDB";
-        answers[1][1] = "int";
-        answers[2][1] = "java.util package";
-        answers[3][1] = "Marker Interface";
-        answers[4][1] = "Heap memory";
-        answers[5][1] = "Remote interface";
-        answers[6][1] = "import";
-        answers[7][1] = "Java Archive";
-        answers[8][1] = "java.lang.StringBuilder";
-        answers[9][1] = "Bytecode is executed by JVM";
-        
+        questions[8][0] = "Gerakan tekanan horizontal yang menyebabkan lapisan kulit bumi berkerut disebut....";
+        questions[8][1] = "a. lipatan";
+        questions[8][2] = "b. patahan";
+        questions[8][3] = "c. gulungan";
+        questions[8][4] = "d. retakan";
+
+        questions[9][0] = "Pelapukan adalah perusakan karena terpengaruh oleh...";
+        questions[9][1] = "a. air, limbah, dan udara";
+        questions[9][2] = "b. batuan, organisme, dan bakteri";
+        questions[9][3] = "c. tumbuhan, hewan, dan manusia";
+        questions[9][4] = "d. temperatur, air, dan organisme";
+
+        answers[0][1] = "b. sial";
+        answers[1][1] = "a. metamorf";
+        answers[2][1] = "d. eksogen";
+        answers[3][1] = "c. vulkanisme";
+        answers[4][1] = "c. dataran rendah";
+        answers[5][1] = "b. mofet";
+        answers[6][1] = "c. stromboli";
+        answers[7][1] = "c. makdani";
+        answers[8][1] = "a. lipatan";
+        answers[9][1] = "d. temperatur, air, dan organisme";
+
         opt1 = new JRadioButton();
         opt1.setBounds(170, 520, 700, 30);
         opt1.setBackground(Color.WHITE);
         opt1.setFont(new Font("Dialog", Font.PLAIN, 20));
         add(opt1);
-        
+
         opt2 = new JRadioButton();
         opt2.setBounds(170, 560, 700, 30);
         opt2.setBackground(Color.WHITE);
         opt2.setFont(new Font("Dialog", Font.PLAIN, 20));
         add(opt2);
-        
+
         opt3 = new JRadioButton();
         opt3.setBounds(170, 600, 700, 30);
         opt3.setBackground(Color.WHITE);
         opt3.setFont(new Font("Dialog", Font.PLAIN, 20));
         add(opt3);
-        
+
         opt4 = new JRadioButton();
         opt4.setBounds(170, 640, 700, 30);
         opt4.setBackground(Color.WHITE);
         opt4.setFont(new Font("Dialog", Font.PLAIN, 20));
         add(opt4);
-        
+
         groupoptions = new ButtonGroup();
         groupoptions.add(opt1);
         groupoptions.add(opt2);
         groupoptions.add(opt3);
         groupoptions.add(opt4);
-        
+
         next = new JButton("Next");
         next.setBounds(1100, 550, 200, 40);
         next.setFont(new Font("Tahoma", Font.PLAIN, 22));
@@ -150,7 +150,7 @@ public class Quiz extends JFrame implements ActionListener {
         next.setForeground(Color.WHITE);
         next.addActionListener(this);
         add(next);
-        
+
         lifeline = new JButton("50-50 Lifeline");
         lifeline.setBounds(1100, 630, 200, 40);
         lifeline.setFont(new Font("Tahoma", Font.PLAIN, 22));
@@ -158,7 +158,7 @@ public class Quiz extends JFrame implements ActionListener {
         lifeline.setForeground(Color.WHITE);
         lifeline.addActionListener(this);
         add(lifeline);
-        
+
         submit = new JButton("Submit");
         submit.setBounds(1100, 710, 200, 40);
         submit.setFont(new Font("Tahoma", Font.PLAIN, 22));
@@ -167,12 +167,12 @@ public class Quiz extends JFrame implements ActionListener {
         submit.addActionListener(this);
         submit.setEnabled(false);
         add(submit);
-        
+
         start(count);
-        
+
         setVisible(true);
     }
-    
+
     public void actionPerformed(ActionEvent ae) {
         if (ae.getSource() == next) {
             repaint();
@@ -180,19 +180,19 @@ public class Quiz extends JFrame implements ActionListener {
             opt2.setEnabled(true);
             opt3.setEnabled(true);
             opt4.setEnabled(true);
-            
+
             ans_given = 1;
             if (groupoptions.getSelection() == null) {
-               useranswers[count][0] = "";
+                useranswers[count][0] = "";
             } else {
                 useranswers[count][0] = groupoptions.getSelection().getActionCommand();
             }
-            
+
             if (count == 8) {
                 next.setEnabled(false);
                 submit.setEnabled(true);
             }
-            
+
             count++;
             start(count);
         } else if (ae.getSource() == lifeline) {
@@ -223,29 +223,29 @@ public class Quiz extends JFrame implements ActionListener {
             new Score(name, score);
         }
     }
-    
+
     public void paint(Graphics g) {
         super.paint(g);
-        
+
         String time = "Time left - " + timer + " seconds"; // 15
         g.setColor(Color.RED);
         g.setFont(new Font("Tahoma", Font.BOLD, 25));
-        
-        if (timer > 0) { 
+
+        if (timer > 0) {
             g.drawString(time, 1100, 500);
         } else {
             g.drawString("Times up!!", 1100, 500);
         }
-        
+
         timer--; // 14
-        
+
         try {
             Thread.sleep(1000);
             repaint();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         if (ans_given == 1) {
             ans_given = 0;
             timer = 15;
@@ -255,18 +255,18 @@ public class Quiz extends JFrame implements ActionListener {
             opt2.setEnabled(true);
             opt3.setEnabled(true);
             opt4.setEnabled(true);
-            
+
             if (count == 8) {
                 next.setEnabled(false);
                 submit.setEnabled(true);
             }
             if (count == 9) { // submit button
                 if (groupoptions.getSelection() == null) {
-                   useranswers[count][0] = "";
+                    useranswers[count][0] = "";
                 } else {
                     useranswers[count][0] = groupoptions.getSelection().getActionCommand();
                 }
-                
+
                 for (int i = 0; i < useranswers.length; i++) {
                     if (useranswers[i][0].equals(answers[i][1])) {
                         score += 10;
@@ -278,7 +278,7 @@ public class Quiz extends JFrame implements ActionListener {
                 new Score(name, score);
             } else { // next button
                 if (groupoptions.getSelection() == null) {
-                   useranswers[count][0] = "";
+                    useranswers[count][0] = "";
                 } else {
                     useranswers[count][0] = groupoptions.getSelection().getActionCommand();
                 }
@@ -286,27 +286,27 @@ public class Quiz extends JFrame implements ActionListener {
                 start(count);
             }
         }
-        
+
     }
-    
+
     public void start(int count) {
         qno.setText("" + (count + 1) + ". ");
         question.setText(questions[count][0]);
         opt1.setText(questions[count][1]);
         opt1.setActionCommand(questions[count][1]);
-        
+
         opt2.setText(questions[count][2]);
         opt2.setActionCommand(questions[count][2]);
-        
+
         opt3.setText(questions[count][3]);
         opt3.setActionCommand(questions[count][3]);
-        
+
         opt4.setText(questions[count][4]);
         opt4.setActionCommand(questions[count][4]);
-        
+
         groupoptions.clearSelection();
     }
-    
+
     public static void main(String[] args) {
         new Quiz("User");
     }
